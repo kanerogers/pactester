37c1eb6ffa4bdef54ac87bcd6afc47d76ff25e1e
Kane Rogers <kane.rogers@bluereef.com.au>
2013-04-30 01:08:28 +1000

    Initial commit.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('pacTesterApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
