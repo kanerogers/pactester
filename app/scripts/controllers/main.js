c5362b9302d8def19e1c070ccc7f3f0e2a934f3a
Kane Rogers <kane.rogers@bluereef.com.au>
2013-05-01 10:58:29 +1000

    Early styling work.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,7 @@
 
 angular.module('pacTesterApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    var check = function() {
+      var pac, domain, source-ip, result;
+    }
   });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,11 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
+/*  it('should attach a list of awesomeThings to the scope', function () {
     expect(scope.awesomeThings.length).toBe(3);
-  });
+  }); */
+
+    it('should have the pac file in scope', function() {
+      expect(scope.pac).toBe('Test');
+    });
 });
